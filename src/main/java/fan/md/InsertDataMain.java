6c4c2a357e5345850582032faec1a94b38364d7f
fan <632947957@qq.com>
2019-12-23 09:41:55 +0800

    add test for yaml

--- src/main/java/fan/md/InsertDataMain.java
+++ src/main/java/fan/md/InsertDataMain.java
@@ -1,26 +1,22 @@
 package fan.md;
 
-import java.io.File;
-import java.util.Map;
-
-import org.ho.yaml.Yaml;
-
 import fan.md.service.DependsCodeInsertService;
 import fan.md.service.InsertDependsCodeToNeo4j;
+import fan.md.utils.YamlUtils;
 
 public class InsertDataMain {
 
-    public static void main1(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
     	insert();
 	}
     
     public static void insert() {
 		try {
-			File file = new File("src/main/resources/application.yml");
-			Map<?, ?> yaml = (Map<?, ?>) Yaml.load(file);
-			String databasePath = (String) ((Map<?, ?>) ((Map<?, ?>) yaml.get("data")).get("neo4j")).get("path");
-			String projectPath = (String) ((Map<?, ?>) ((Map<?, ?>) yaml.get("data")).get("code")).get("path");
-			String language = (String) ((Map<?, ?>) ((Map<?, ?>) yaml.get("data")).get("code")).get("language");
+			YamlUtils.YamlObject yaml = YamlUtils.getDataBasePath("src/main/resources/application.yml");
+			String databasePath = yaml.getNeo4jDatabasePath();
+			String projectPath = yaml.getCodeProjectPath();
+			String language = yaml.getCodeLanguage();
+			
 			InsertDependsCodeToNeo4j codeExtractor = DependsCodeInsertService.getInstance();
 			codeExtractor.extractEntityRepo(projectPath, language);
 			codeExtractor.insertCodeToNeo4jDataBase(databasePath, true);
---
+++ src/test/java/fan/md/utils/YamlUtilsTest.java
@@ -0,0 +1,22 @@
+package fan.md.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class YamlUtilsTest {
+
+	@Test
+	public void test() {
+		YamlUtils.YamlObject yaml;
+		try {
+			yaml = YamlUtils.getDataBasePath("src/main/resources/application.yml");
+			String test = yaml.forTest;
+			assertTrue("this property is for YamlUtilsTest".equals(test));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
