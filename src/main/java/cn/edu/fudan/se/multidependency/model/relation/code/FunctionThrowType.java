65c6729f424d30b875bac28835152fe8124048ae
fan <632947957@qq.com>
2020-01-03 10:46:07 +0800

    update

---
+++ src/main/java/cn/edu/fudan/se/multidependency/model/relation/code/FunctionThrowType.java
@@ -0,0 +1,84 @@
+package cn.edu.fudan.se.multidependency.model.relation.code;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.neo4j.ogm.annotation.GeneratedValue;
+import org.neo4j.ogm.annotation.Id;
+import org.neo4j.ogm.annotation.RelationshipEntity;
+
+import cn.edu.fudan.se.multidependency.model.node.code.Function;
+import cn.edu.fudan.se.multidependency.model.node.code.Type;
+import cn.edu.fudan.se.multidependency.model.relation.Relation;
+import cn.edu.fudan.se.multidependency.model.relation.RelationType;
+
+@RelationshipEntity(RelationType.str_FUNCTION_THORW_TYPE)
+public class FunctionThrowType implements Relation {
+
+	private static final long serialVersionUID = -5858763297137981586L;
+	
+	@Id
+    @GeneratedValue
+    private Long id;
+	
+	@Override
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public FunctionThrowType() {
+		super();
+	}
+
+	public FunctionThrowType(Function function, Type type) {
+		super();
+		this.function = function;
+		this.type = type;
+	}
+
+	private Function function;
+	
+	private Type type;
+
+	@Override
+	public Long getStartNodeGraphId() {
+		return function.getId();
+	}
+
+	@Override
+	public Long getEndNodeGraphId() {
+		return type.getId();
+	}
+
+	@Override
+	public RelationType getRelationType() {
+		return RelationType.FUNCTION_THROW_TYPE;
+	}
+
+	@Override
+	public Map<String, Object> getProperties() {
+		return new HashMap<>();
+	}
+
+	public Function getFunction() {
+		return function;
+	}
+
+	public void setFunction(Function function) {
+		this.function = function;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public void setType(Type type) {
+		this.type = type;
+	}
+
+}
