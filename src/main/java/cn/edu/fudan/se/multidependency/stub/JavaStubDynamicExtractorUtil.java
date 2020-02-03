85abd369e74efa88e725721dd9ea04a71a4a3169
fan <632947957@qq.com>
2020-02-03 16:59:01 +0800

    插桩日志提取

--- src/main/java/cn/edu/fudan/se/multidependency/stub/JavaStubDynamicExtractorUtil.java
+++ src/main/java/cn/edu/fudan/se/multidependency/stub/JavaStubDynamicExtractorUtil.java
@@ -8,14 +8,18 @@ import lombok.Data;
 public class JavaStubDynamicExtractorUtil {
 	
 	
-	public static DynamicFunctionExecutionFromStub extract(String sentence, String projectName) {
+	public static DynamicFunctionExecutionFromStub extract(String sentence) {
 		try {
 			DynamicFunctionExecutionFromStub functionExecution = new DynamicFunctionExecutionFromStub();
 			functionExecution.setSentence(sentence);
 			String[] splitFilePath = sentence.split("\\|");
-			String filePath = splitFilePath[0];
+			String time = splitFilePath[0];
+			functionExecution.setTime(time);
+			String projectName = splitFilePath[1];
+			functionExecution.setProjectName(projectName);
+			String filePath = splitFilePath[2];
 			functionExecution.setFilePath(filePath);
-			String executionInfo = splitFilePath[1];
+			String executionInfo = splitFilePath[3];
 			String[] splitExecutionInfos = executionInfo.split("-");
 			functionExecution.setLayer(Long.parseLong(splitExecutionInfos[3]));
 			functionExecution.setOrder(Long.parseLong(splitExecutionInfos[2]));
@@ -27,27 +31,26 @@ public class JavaStubDynamicExtractorUtil {
 			}
 			return functionExecution;
 		} catch (Exception e) {
+			e.printStackTrace();
 			return null;
 		}
 	}
 	
 	public static void main(String[] args) {
-		String str = "D:\\multiple-dependency-project\\depends\\src\\main\\java\\depends\\relations\\RelationCounter.java|depends.relations.RelationCounter.RelationCounter-(Collection<Entity>, Inferer, EntityRepo, boolean, AbstractLangProcessor)-939-3";
-//		str = "D:\\multiple-dependency-project\\depends\\src\\main\\java\\depends\\relations\\RelationCounter.java|depends.relations.RelationCounter.RelationCounter-(Collection<Entity>)-939-3";
-//		str = "D:\\multiple-dependency-project\\depends\\src\\main\\java\\depends\\relations\\RelationCounter.java|depends.relations.RelationCounter.RelationCounter-()-939-3";
-		System.out.println(extract(str, "depends"));
+		String str = "2020/02/03-14:33:41|depends|D:\\multiple-dependency-project\\depends\\target\\generated-sources\\antlr4\\depends\\extractor\\java\\JavaParserBaseListener.java|depends.extractor.java.JavaParserBaseListener.exitEveryRule-(ParserRuleContext)-251-2";
+		System.out.println(extract(str));
 	}
 	
 	@Data
 	public static class DynamicFunctionExecutionFromStub { //读取到的方法，所具有的属性
-		String sentence;
+		String time;
+		String projectName;
 		String filePath;
 		Long order;
 		Long layer;
 		String functionName;
 		List<String> parameterTypes = new ArrayList<>();
-		String time;
-		String projectName;
+		String sentence;
 		public void addParameterType(String parameterType) {
 			this.parameterTypes.add(parameterType);
 		}
