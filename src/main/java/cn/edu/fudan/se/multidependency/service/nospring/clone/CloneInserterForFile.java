11fcc9532cf0b4246bfbc6d182006cb4d4031d3e
fan <632947957@qq.com>
2020-05-27 12:16:33 +0800

    克隆分析转为多线程运行

--- src/main/java/cn/edu/fudan/se/multidependency/service/nospring/clone/CloneInserterForFile.java
+++ src/main/java/cn/edu/fudan/se/multidependency/service/nospring/clone/CloneInserterForFile.java
@@ -1,6 +1,9 @@
 package cn.edu.fudan.se.multidependency.service.nospring.clone;
 
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +19,10 @@ import lombok.Setter;
 
 public class CloneInserterForFile extends ExtractorForNodesAndRelationsImpl {
 
+	private static final Executor executor = Executors.newCachedThreadPool();
+	
+	private CountDownLatch latch;
+	
 	private static final Logger LOGGER = LoggerFactory.getLogger(CloneUtil.class);
 	@Setter
 	private Language language;
@@ -29,40 +36,61 @@ public class CloneInserterForFile extends ExtractorForNodesAndRelationsImpl {
 		this.language = language;
 		this.namePath = namePath;
 		this.resultPath = resultPath;
+		this.latch = new CountDownLatch(2);
 	}
+	
+	private Map<Integer, FilePathForJavaFromCsv> filePaths;
+	private Iterable<CloneResultFromCsv> cloneResults;
 
 	@Override
 	public void addNodesAndRelations() throws Exception {
-		if(Language.java == language) {
-			Map<Integer, FilePathForJavaFromCsv> filePaths = CloneUtil.readJavaCloneCsvForFilePath(namePath);
-			Iterable<CloneResultFromCsv> cloneResults = CloneUtil.readCloneResultCsv(resultPath);
-			
-			for(CloneResultFromCsv cloneResult : cloneResults) {
-				int start = cloneResult.getStart();
-				int end = cloneResult.getEnd();
-				double value = cloneResult.getValue();
-				FilePathForJavaFromCsv filePath1 = filePaths.get(start);
-				if(filePath1 == null) {
-					throw new Exception("filePath1 is null");
-				}
-				FilePathForJavaFromCsv filePath2 = filePaths.get(end);
-				if(filePath2 == null) {
-					throw new Exception("filePath2 is null");
-				}
-				ProjectFile file1 = this.getNodes().findFileByPathRecursion(filePath1.getFilePath());
-				ProjectFile file2 = this.getNodes().findFileByPathRecursion(filePath2.getFilePath());
-				if(file1 == null) {
-					LOGGER.warn("file1 is null " + filePath1.getFilePath());
-					continue;
-				}
-				if(file2 == null) {
-					LOGGER.warn("file2 is null " + filePath2.getFilePath());
-					continue;
-				}
-				FileCloneFile clone = new FileCloneFile(file1, file2);
-				clone.setValue(value);
-				addRelation(clone);
+		executor.execute(() -> {
+			try {
+				filePaths = CloneUtil.readJavaCloneCsvForFilePath(namePath);
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				latch.countDown();
 			}
+		});
+		
+		executor.execute(() -> {
+			try {
+				cloneResults = CloneUtil.readCloneResultCsv(resultPath);
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				latch.countDown();
+			}
+		});
+		
+		latch.await();
+		
+		for(CloneResultFromCsv cloneResult : cloneResults) {
+			int start = cloneResult.getStart();
+			int end = cloneResult.getEnd();
+			double value = cloneResult.getValue();
+			FilePathForJavaFromCsv filePath1 = filePaths.get(start);
+			if(filePath1 == null) {
+				throw new Exception("filePath1 is null");
+			}
+			FilePathForJavaFromCsv filePath2 = filePaths.get(end);
+			if(filePath2 == null) {
+				throw new Exception("filePath2 is null");
+			}
+			ProjectFile file1 = this.getNodes().findFileByPathRecursion(filePath1.getFilePath());
+			ProjectFile file2 = this.getNodes().findFileByPathRecursion(filePath2.getFilePath());
+			if(file1 == null) {
+				LOGGER.warn("file1 is null " + filePath1.getFilePath());
+				continue;
+			}
+			if(file2 == null) {
+				LOGGER.warn("file2 is null " + filePath2.getFilePath());
+				continue;
+			}
+			FileCloneFile clone = new FileCloneFile(file1, file2);
+			clone.setValue(value);
+			addRelation(clone);
 		}
 	}
 
