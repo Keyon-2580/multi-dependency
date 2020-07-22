e1ee4a0d0aec9697c8bbb19e1f6cc0f0da1e456a
fan <632947957@qq.com>
2020-07-22 15:58:33 +0800

    將hublike的package按Project分别计算

--- src/main/java/cn/edu/fudan/se/multidependency/service/spring/as/impl/HubLikeComponentDetectorImpl.java
+++ src/main/java/cn/edu/fudan/se/multidependency/service/spring/as/impl/HubLikeComponentDetectorImpl.java
@@ -2,13 +2,17 @@ package cn.edu.fudan.se.multidependency.service.spring.as.impl;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import cn.edu.fudan.se.multidependency.model.node.Package;
+import cn.edu.fudan.se.multidependency.model.node.Project;
 import cn.edu.fudan.se.multidependency.repository.node.PackageRepository;
+import cn.edu.fudan.se.multidependency.service.spring.ContainRelationService;
 import cn.edu.fudan.se.multidependency.service.spring.as.HubLikeComponentDetector;
 import cn.edu.fudan.se.multidependency.service.spring.metric.FanIOMetric;
 import cn.edu.fudan.se.multidependency.service.spring.metric.PackageMetrics;
@@ -18,18 +22,54 @@ public class HubLikeComponentDetectorImpl implements HubLikeComponentDetector {
 	
 	@Autowired
 	private PackageRepository packageRepository;
+	
+	@Autowired
+	private ContainRelationService containRelationService;
 
 	@Override
 	public Collection<Package> hubLikePackages() {
 		List<Package> result = new ArrayList<>();
-		List<PackageMetrics> packageMetrics = packageRepository.calculatePackageMetrics();
+		/*List<PackageMetrics> packageMetrics = packageRepository.calculatePackageMetrics();
 		int fanOutMedian = calculateFanOutMedian(packageMetrics);
 		int fanInMedian = calculateFanInMedian(packageMetrics);
 		for(PackageMetrics metric : packageMetrics) {
 			if(isHubLikeComponent(metric, fanOutMedian, fanInMedian)) {
 				result.add(metric.getPck());
 			}
+		}*/
+		Map<Project, List<Package>> projectToHubLikePackages = hubLikePackagesInDifferentProject();
+		for(Map.Entry<Project, List<Package>> entry : projectToHubLikePackages.entrySet()) {
+			result.addAll(entry.getValue());
 		}
+		return result;
+	}
+	
+	public Map<Project, List<Package>> hubLikePackagesInDifferentProject() {
+		Map<Project, List<Package>> result = new HashMap<>();
+		List<PackageMetrics> packageMetrics = packageRepository.calculatePackageMetrics();
+		Map<Project, List<PackageMetrics>> projectToPackageMetrics = new HashMap<>();
+		for(PackageMetrics metric : packageMetrics) {
+			Package pck = metric.getPck();
+			Project project = containRelationService.findPackageBelongToProject(pck);
+			List<PackageMetrics> temp = projectToPackageMetrics.getOrDefault(project, new ArrayList<>());
+			temp.add(metric);
+			projectToPackageMetrics.put(project, temp);
+		}
+		
+		for(Map.Entry<Project, List<PackageMetrics>> entry : projectToPackageMetrics.entrySet()) {
+			Project project = entry.getKey();
+			List<PackageMetrics> metrics = entry.getValue();
+			int fanOutMedian = calculateFanOutMedian(metrics);
+			int fanInMedian = calculateFanInMedian(metrics);
+			List<Package> temp = new ArrayList<>();
+			for(PackageMetrics metric : metrics) {
+				if(isHubLikeComponent(metric, fanOutMedian, fanInMedian)) {
+					temp.add(metric.getPck());
+				}
+			}
+			result.put(project, temp);
+		}
+		
 		return result;
 	}
 	
