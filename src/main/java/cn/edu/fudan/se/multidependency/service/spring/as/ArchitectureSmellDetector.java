dff5d4eee05bb350d92bc450a9f3f509c8bebded
fan <632947957@qq.com>
2020-07-23 15:30:56 +0800

    添加架构异味检测类

---
+++ src/main/java/cn/edu/fudan/se/multidependency/service/spring/as/ArchitectureSmellDetector.java
@@ -0,0 +1,23 @@
+package cn.edu.fudan.se.multidependency.service.spring.as;
+
+import java.util.Collection;
+
+import cn.edu.fudan.se.multidependency.model.node.Package;
+import cn.edu.fudan.se.multidependency.model.relation.DependOn;
+import cn.edu.fudan.se.multidependency.service.spring.metric.FileMetrics;
+import cn.edu.fudan.se.multidependency.service.spring.metric.PackageMetrics;
+
+public interface ArchitectureSmellDetector {
+	
+	/**
+	 * 包的循环依赖的检测
+	 * @return
+	 */
+	Collection<Collection<DependOn>> cyclePackages();
+	
+	Collection<Package> unusedPackages();
+	
+	Collection<PackageMetrics> hubLikePackages();
+	
+	Collection<FileMetrics> hubLikeFiles();
+}
