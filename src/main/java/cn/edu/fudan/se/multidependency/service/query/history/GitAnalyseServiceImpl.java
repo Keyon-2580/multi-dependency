b7d12f87de7945c2ec0da14e03b8d127ffe93bf1
Kingsley <2901704946@qq.com>
2020-09-29 15:00:07 +0800

    调整cochange显示页面跳转

--- src/main/java/cn/edu/fudan/se/multidependency/service/query/history/GitAnalyseServiceImpl.java
+++ src/main/java/cn/edu/fudan/se/multidependency/service/query/history/GitAnalyseServiceImpl.java
@@ -78,7 +78,7 @@ public class GitAnalyseServiceImpl implements GitAnalyseService {
 		for(GitRepository gitRepository : gitRepoRepository.findAll()) {
 			GitRepoMetric gitRepoMetric = new GitRepoMetric();
 			gitRepoMetric.setGitRepository(gitRepository);
-			Collection<ProjectMetrics> ProjectMetricList = metricCalculator.calculateProjectMetrics(false).values();
+			Collection<ProjectMetrics> ProjectMetricList = metricCalculator.calculateProjectMetricsByGitRepository(false, gitRepository).values();
 			gitRepoMetric.setProjectMetricsList(ProjectMetricList);
 			gitRepoMetric.setNumOfCommits(calculateGitRepoCommits(gitRepository));
 			gitRepoMetric.setNumOfIssues(0);
