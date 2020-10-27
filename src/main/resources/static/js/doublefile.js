5dd7c249068eed69fb998069df8d693be760b46f
Kingsley <2901704946@qq.com>
2020-10-27 13:10:32 +0800

    调整Type跳转页面格式和接口

--- src/main/resources/static/js/doublefile.js
+++ src/main/resources/static/js/doublefile.js
@@ -17,33 +17,34 @@ var doublefile = function() {
         zTreeObj = $.fn.zTree.init($("#" + divId), setting, zNodes);
     }
 
-    var loaddata = function(fileId1,fileId2,cloneType,linesSize1,linesSize2,loc1,loc2,value,cochange,filePath1,filePath2,cochangeId) {
-        // showtree("tree_file1", data1);
-        // showtree("tree_file2", data2);
+    var loaddata = function(file1Id,file2Id,cloneType,linesSize1,linesSize2,loc1,loc2,value,cochange,filePath1,filePath2,cochangeId) {
         $.ajax({
             type : "GET",
-            url : "/clone/file/double/json?fileId1="+ fileId1 +"&fileId2=" + fileId2,
+            url : "/clone/file/double/json?fileId1="+ file1Id +"&fileId2=" + file2Id,
             success : function(result) {
                 var html = ""
-                html += "<table class = \"gridtable\">"
-                    + "<tr><th>ID</th><th>filePath</th><th>cloneType</th><th>linesSize</th><th>loc</th></tr>"
-                    + "<tr><td>1</td><td>" + filePath1 + "</td><td>" + cloneType + "</td><td>" + linesSize1 +"</td><td>" + loc1 +"</td></tr>"
-                    + "<tr><td>2</td><td>" + filePath2 + "</td><td>" + cloneType + "</td><td>" + linesSize2 +"</td><td>" + loc2 +"</td></tr>";
+                html += "<table class = 'table table-bordered'>"
+                html += "<tr><th>ID</th><th>filePath</th><th>linesSize</th><th>loc</th><th>type</th><th>value</th><th>cochange</th></tr>"
+                html += "<tr>";
+                html += "<td>"+ file1Id + "</td>";
+                html += "<td>" + filePath1 + "</td>";
+                html += "<td>" + linesSize1 +"</td>";
+                html += "<td>" + loc1 +"</td>";
+                html += "<td rowspan='2' style='vertical-align: middle'>" + cloneType + "</td>";
+                html += "<td rowspan='2' style='vertical-align: middle'><a target='_blank' href='/clone/compare?id1=" + file1Id + "&id2=" + file2Id + "'>" + value + "</a></td>";
+                html += "<td rowspan='2' style='vertical-align: middle'><a target='_blank' href='/commit/cochange?cochangeId=" + cochangeId + "'>" + cochange + "</a></td>";
+                html += "</tr>"
+                html += "<tr>";
+                html += "<td>" + file2Id + "</td>";
+                html += "<td>" + filePath2 + "</td>"
+                html += "<td>" + linesSize2 +"</td>";
+                html += "<td>" + loc2 +"</td>";
+                html += "</tr>";
                 html += "</table>";
-
-                html += "<table class = \"gridtable\" style = \"margin-top: 20px\">"
-                    + "<tr><th>cochange</th><th>value</th></tr>"
-                    + "<tr><td><a class='cochangeTimes' target='_blank' href='/commit/cochange?cochangeId=" + cochangeId
-                    + "'>" + cochange + "</a></td><td><a target='_blank' href='/clone/compare?id1=" + fileId1 + "&id2=" + fileId2 +"'>" + value + "</td></tr>";
-                html += "</table>";
-                // console.log(html)
-
                 $("#file_table").html(html);
-
-                var data1 = result[fileId1];
-                // console.log()
+                var data1 = result[file1Id];
                 showtree("tree_file1", data1);
-                var data2 = result[fileId2];
+                var data2 = result[file2Id];
                 showtree("tree_file2", data2);
 
             }
@@ -51,8 +52,8 @@ var doublefile = function() {
     }
 
     return {
-        init : function(fileId1,fileId2,cloneType,linesSize1,linesSize2,loc1,loc2,value,cochange,filePath1,filePath2,cochangeId) {
-            loaddata(fileId1,fileId2,cloneType,linesSize1,linesSize2,loc1,loc2,value,cochange,filePath1,filePath2,cochangeId);
+        init : function(file1Id,file2Id,cloneType,linesSize1,linesSize2,loc1,loc2,value,cochange,filePath1,filePath2,cochangeId) {
+            loaddata(file1Id,file2Id,cloneType,linesSize1,linesSize2,loc1,loc2,value,cochange,filePath1,filePath2,cochangeId);
         }
     }
 }
