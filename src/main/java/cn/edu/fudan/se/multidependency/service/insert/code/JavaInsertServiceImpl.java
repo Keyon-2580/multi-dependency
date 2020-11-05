6eff91f418fe40b2c2680fea3adf9bb7c0b1430b
daihongok <daihongok@163.com>
2020-11-05 14:05:04 +0800

    update type identify name of variable

--- src/main/java/cn/edu/fudan/se/multidependency/service/insert/code/JavaInsertServiceImpl.java
+++ src/main/java/cn/edu/fudan/se/multidependency/service/insert/code/JavaInsertServiceImpl.java
@@ -43,7 +43,7 @@ public class JavaInsertServiceImpl extends DependsCodeInserterForNeo4jServiceImp
 		file.setPath(filePath);
 		file.setSuffix(FileUtil.extractSuffix(entity.getQualifiedName()));
 		file.setEndLine(entity.getEndLine() == null ? -1 : entity.getEndLine());
-		file.setLoc(entity.getLoc()+1);
+		file.setLoc(entity.getLoc() == null ? -1 : entity.getLoc());
 		addNode(file, currentProject);
 		
 		// 文件所在目录
@@ -101,13 +101,8 @@ public class JavaInsertServiceImpl extends DependsCodeInserterForNeo4jServiceImp
 		variable.setEntityId(entity.getId().longValue());
 		String typeIdentify = ((VarEntity) entity).getRawType().getName();
 		List<GenericName> varArguments = ((VarEntity) entity).getRawName().getArguments();
-		if(varArguments != null && !varArguments.isEmpty()){
-			typeIdentify += "<";
-			for (GenericName arg : varArguments){
-				typeIdentify += arg.getName() + ",";
-			}
-			typeIdentify = typeIdentify.substring(0,typeIdentify.length()-1);
-			typeIdentify += ">";
+		if (varArguments != null && !varArguments.isEmpty()){
+			typeIdentify += getTypeIdentifyOfVar(varArguments);
 		}
 		variable.setTypeIdentify(typeIdentify);
 		variable.setName(entity.getQualifiedName());
@@ -115,6 +110,7 @@ public class JavaInsertServiceImpl extends DependsCodeInserterForNeo4jServiceImp
 		addNode(variable, currentProject);
 		return variable;
 	}
+
 	
 	private Type process(TypeEntity entity) {
 		Type type = new Type();
