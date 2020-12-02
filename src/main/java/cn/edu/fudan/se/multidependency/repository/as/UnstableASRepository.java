b588662262b2acb26ab33c7a97ae6bf02136de83
daihongok <daihongok@163.com>
2020-12-02 20:04:31 +0800

    update unstable as detection with instability

--- src/main/java/cn/edu/fudan/se/multidependency/repository/as/UnstableASRepository.java
+++ src/main/java/cn/edu/fudan/se/multidependency/repository/as/UnstableASRepository.java
@@ -17,8 +17,8 @@ public interface UnstableASRepository extends Neo4jRepository<ProjectFile, Long>
 
 	@Query("match (project:Project)-[:CONTAIN*2]->(file:ProjectFile) where id(project)={projectId} and file.fanOut >= {fanOut} "
 			+ "with file match (file)-[:DEPENDS_ON]->(dependsFile:ProjectFile) "
-			+ "where dependsFile.instability > file.instability "
-			+ "with file, count(dependsFile) / 2 as badDependencies, file.fanOut as allDependencies "
+			+ "where dependsFile.instability > file.instability and dependsFile.fanOut >= {fanOut} "
+			+ "with file, count(dependsFile) as badDependencies, file.fanOut as allDependencies "
 			+ "with file, badDependencies, allDependencies "
 			+ "where ((badDependencies + 0.0) / allDependencies) >= {ratio} "
 			+ "return file as component, file.instability as instability, badDependencies, allDependencies")
@@ -27,7 +27,7 @@ public interface UnstableASRepository extends Neo4jRepository<ProjectFile, Long>
 	
 	@Query("match (project:Project)-[:CONTAIN]->(pck:Package) where id(project)={projectId} and pck.fanOut >= {fanOut} "
 			+ "with pck match (pck)-[:DEPENDS_ON]->(dependsPck:Package) "
-			+ "where dependsPck.instability > pck.instability "
+			+ "where dependsPck.instability > pck.instability and dependsPck.fanOut >= {fanOut} "
 			+ "with pck, count(dependsPck) as badDependencies, pck.fanOut as allDependencies "
 			+ "with pck, badDependencies, allDependencies "
 			+ "where ((badDependencies + 0.0) / allDependencies) >= {ratio} "
@@ -37,7 +37,7 @@ public interface UnstableASRepository extends Neo4jRepository<ProjectFile, Long>
 	
 	@Query("match (project:Project)-[:CONTAIN]->(module:Module) where id(project)={projectId} and module.fanOut >= {fanOut} "
 			+ "with module match (module)-[:DEPENDS_ON]->(dependsModule:Module) "
-			+ "where dependsModule.instability > module.instability "
+			+ "where dependsModule.instability > module.instability and dependsModule.fanOut >= {fanOut} "
 			+ "with module, count(dependsModule) as badDependencies, module.fanOut as allDependencies "
 			+ "with module, badDependencies, allDependencies "
 			+ "where ((badDependencies + 0.0) / allDependencies) >= {ratio} "
