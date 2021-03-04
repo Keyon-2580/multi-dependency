925e1e3dd12cf0f7761261a0f850025a271e4c46
daihongok <daihongok@163.com>
2021-03-04 22:51:57 +0800

    update cyclic dependency smell saving mode

--- src/main/java/cn/edu/fudan/se/multidependency/service/query/smell/SmellDetectorService.java
+++ src/main/java/cn/edu/fudan/se/multidependency/service/query/smell/SmellDetectorService.java
@@ -1,7 +1,13 @@
 package cn.edu.fudan.se.multidependency.service.query.smell;
 
+import cn.edu.fudan.se.multidependency.model.node.Project;
 import cn.edu.fudan.se.multidependency.model.node.ProjectFile;
 import cn.edu.fudan.se.multidependency.model.node.smell.Smell;
+import cn.edu.fudan.se.multidependency.model.node.smell.SmellLevel;
+import cn.edu.fudan.se.multidependency.model.node.smell.SmellType;
+import cn.edu.fudan.se.multidependency.model.relation.Contain;
+import cn.edu.fudan.se.multidependency.repository.node.ProjectRepository;
+import cn.edu.fudan.se.multidependency.repository.relation.ContainRepository;
 import cn.edu.fudan.se.multidependency.repository.smell.ModuleRepository;
 import cn.edu.fudan.se.multidependency.repository.smell.SmellRepository;
 import cn.edu.fudan.se.multidependency.service.query.CacheService;
@@ -10,6 +16,7 @@ import cn.edu.fudan.se.multidependency.service.query.smell.data.Cycle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,9 +29,13 @@ public class SmellDetectorService {
 	
 	@Autowired
 	private CacheService cache;
-	
+
 	@Autowired
-	private StaticAnalyseService staticAnalyseService;
+	private ProjectRepository projectRepository;
+
+	@Autowired
+	private ContainRepository containRepository;
+
 
 	@Autowired
 	private CyclicDependencyDetector cyclicDependencyDetector;
@@ -33,22 +44,47 @@ public class SmellDetectorService {
 	private SmellRepository smellRepository;
 
 	public void createCloneSmells(){
+        smellRepository.deleteSmellContainRelations(SmellType.CLONE);
+        smellRepository.deleteSmellHasMetricRelation(SmellType.CLONE);
+        smellRepository.deleteSmells(SmellType.CLONE);
+
 		smellRepository.createCloneSmells();
 		smellRepository.createCloneSmellContains();
 		smellRepository.setSmellProject();
+	}
 
+	public void createCycleDependencySmells(){
+		smellRepository.deleteSmellContainRelations(SmellType.CYCLIC_DEPENDENCY);
+		smellRepository.deleteSmellHasMetricRelation(SmellType.CYCLIC_DEPENDENCY);
+		smellRepository.deleteSmells(SmellType.CYCLIC_DEPENDENCY);
 		Map<Long, Map<Integer, Cycle<ProjectFile>>> fileCyclicDependencies = new HashMap<>(cyclicDependencyDetector.fileCycles());
 		String name = "file_cycle_";
+		List<Smell> smells = new ArrayList<>();
+		List<Contain> smellContains = new ArrayList<>();
 		for (Map.Entry<Long, Map<Integer, Cycle<ProjectFile>>> fileCyclicDependency : fileCyclicDependencies.entrySet()){
 			long projectId = fileCyclicDependency.getKey();
+			Project project = (Project)projectRepository.queryNodeById(projectId);
 			Map<Integer, Cycle<ProjectFile>> fileCycles = fileCyclicDependency.getValue();
 			for (Map.Entry<Integer, Cycle<ProjectFile>> fileCycle : fileCycles.entrySet()){
 				List<ProjectFile> files = fileCycle.getValue().getComponents();
-				Smell smell = smellRepository.createFileCyclicDependencySmell(name + fileCycle.getKey().toString(), files.size(), files.get(0).getLanguage(), projectId);
+				Smell smell = new Smell();
+				smell.setName(name + fileCycle.getKey().toString());
+				smell.setSize(files.size());
+				smell.setLanguage(files.get(0).getLanguage());
+				smell.setProjectId(projectId);
+				smell.setProjectName(project.getName());
+				smell.setType(SmellType.CYCLIC_DEPENDENCY);
+				smell.setLevel(SmellLevel.FILE);
+//				Smell smell = smellRepository.createFileCyclicDependencySmell(, files.get(0).getLanguage(), projectId);
+				smells.add(smell);
 				for (ProjectFile file : files) {
-					smellRepository.createFileCyclicDependencySmellContains(smell.getId(), file.getId());
+					Contain contain = new Contain(smell,file);
+					smellContains.add(contain);
+//					smellRepository.createFileCyclicDependencySmellContains(smell.getId(), file.getId());
 				}
 			}
 		}
+		smellRepository.saveAll(smells);
+		containRepository.saveAll(smellContains);
 	}
 }
