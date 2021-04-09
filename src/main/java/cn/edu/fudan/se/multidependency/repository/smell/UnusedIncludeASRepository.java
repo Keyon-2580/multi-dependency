da1e0c1b34bffc284781de7ddcc6fdcca92aafc9
Kingsley <2901704946@qq.com>
2021-04-09 19:46:28 +0800

    更新UnusedInclude异味检测算法

--- src/main/java/cn/edu/fudan/se/multidependency/repository/smell/UnusedIncludeASRepository.java
+++ src/main/java/cn/edu/fudan/se/multidependency/repository/smell/UnusedIncludeASRepository.java
@@ -2,6 +2,7 @@ package cn.edu.fudan.se.multidependency.repository.smell;
 
 import cn.edu.fudan.se.multidependency.model.node.ProjectFile;
 import cn.edu.fudan.se.multidependency.model.relation.RelationType;
+import cn.edu.fudan.se.multidependency.service.query.smell.data.UnusedInclude;
 import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.repository.query.Param;
@@ -12,9 +13,17 @@ import java.util.Set;
 @Repository
 public interface UnusedIncludeASRepository extends Neo4jRepository<ProjectFile, Long> {
 
-	@Query("MATCH (file1:ProjectFile)-[:" + RelationType.str_INCLUDE + "]->(file2:ProjectFile),(file1)-[r:DEPENDS_ON]->(file2) where r.times - r.`dependsOnTypes.INCLUDE` <= 0 return collect(distinct file1);")
-	public Set<ProjectFile> findFileWithUnusedInclude();
+	@Query("MATCH (file1:ProjectFile)-[:" + RelationType.str_INCLUDE + "]->(file2:ProjectFile),(file1)-[r:DEPENDS_ON]->(file2) " +
+			"where file1.suffix = $suffix and r.times - r.`dependsOnTypes.INCLUDE` <= 0 " +
+			"with file1 as coreFile, collect(distinct file2) as unusedIncludeFiles " +
+			"return coreFile, unusedIncludeFiles;")
+	public Set<UnusedInclude> findFileWithUnusedIncludeBySuffix(@Param("suffix") String suffix);
 
-	@Query("MATCH (file1:ProjectFile)-[:" + RelationType.str_INCLUDE + "]->(file2:ProjectFile),(file1)-[r:DEPENDS_ON]->(file2) where id(file1) = $fileId and r.times - r.`dependsOnTypes.INCLUDE` <= 0 return collect(distinct file2);")
-	public Set<ProjectFile> findUnusedIncludeByFileId(@Param("fileId") Long fileId);
+	@Query("MATCH (file1:ProjectFile)-[:" + RelationType.str_INCLUDE + "]->(file2:ProjectFile) where id(file2) = $fileId return collect(distinct file1);")
+	public Set<ProjectFile> findFileByHeadFileId(@Param("fileId") Long fileId);
+
+	@Query("MATCH (file1:ProjectFile)-[r:" + RelationType.str_DEPENDS_ON + "]->(file2:ProjectFile) where id(file1) = $file1Id and id(file2) = $file2Id return r.times - r.`dependsOnTypes.INCLUDE` > 0;")
+	public default boolean isUsedInclude(@Param("file1Id") Long file1Id, @Param("file2Id") Long file2Id) {
+		return false;
+	}
 }
--- src/main/java/cn/edu/fudan/se/multidependency/service/query/smell/impl/UnusedIncludeDetectorImpl.java
+++ src/main/java/cn/edu/fudan/se/multidependency/service/query/smell/impl/UnusedIncludeDetectorImpl.java
@@ -29,12 +29,53 @@ public class UnusedIncludeDetectorImpl implements UnusedIncludeDetector {
     @Override
     public Map<Long, List<UnusedInclude>> detectUnusedInclude() {
         Map<Long, List<UnusedInclude>> result = new HashMap<>();
-        Set<ProjectFile> files = unusedIncludeASRepository.findFileWithUnusedInclude();
-        for (ProjectFile file : files) {
-            Project project = containRelationService.findFileBelongToProject(file);
+        Set<UnusedInclude> headFileUnusedIncludeSet = new HashSet<>();
+        Set<UnusedInclude> codeFileUnusedIncludeSet = new HashSet<>();
+        headFileUnusedIncludeSet.addAll(unusedIncludeASRepository.findFileWithUnusedIncludeBySuffix(".h"));
+        headFileUnusedIncludeSet.addAll(unusedIncludeASRepository.findFileWithUnusedIncludeBySuffix(".hpp"));
+        codeFileUnusedIncludeSet.addAll(unusedIncludeASRepository.findFileWithUnusedIncludeBySuffix(".c"));
+        codeFileUnusedIncludeSet.addAll(unusedIncludeASRepository.findFileWithUnusedIncludeBySuffix(".cc"));
+        codeFileUnusedIncludeSet.addAll(unusedIncludeASRepository.findFileWithUnusedIncludeBySuffix(".cpp"));
+        for (UnusedInclude headFileUnusedInclude : headFileUnusedIncludeSet) {
+            ProjectFile headFile = headFileUnusedInclude.getCoreFile();
+            //无后缀名的头文件名
+            String headFileNameWithoutSuffix = headFile.getName().substring(0, headFile.getName().length() - headFile.getSuffix().length());
+            Set<ProjectFile> allFiles = new HashSet<>(unusedIncludeASRepository.findFileByHeadFileId(headFile.getId()));
+            Set<ProjectFile> codeFiles = new HashSet<>();
+            //获取当前头文件对应的源文件，条件：名字相同、引用该同文件、后缀为.c/.cc/.cpp
+            for (ProjectFile file : allFiles) {
+                String codeFileNameWithoutSuffix = file.getName().substring(0, file.getName().length() - file.getSuffix().length());
+                if (headFileNameWithoutSuffix.equals(codeFileNameWithoutSuffix) && (file.getSuffix().equals(".c") || file.getSuffix().equals(".cc") || file.getSuffix().equals(".cpp"))) {
+                    codeFiles.add(file);
+                }
+            }
+            if (!codeFiles.isEmpty()) {
+                //任取一个元素做为该头文件的对应源文件
+                ProjectFile codeFile = codeFiles.iterator().next();
+                Set<ProjectFile> headFileUnusedIncludeFileSet = new HashSet<>(headFileUnusedInclude.getUnusedIncludeFiles());
+                Set<ProjectFile> codeFileUnusedIncludeFileSet = new HashSet<>();
+                //获取该头文件无效引用中在源文件依旧无效的引用
+                for (ProjectFile headFileUnusedIncludeFile : headFileUnusedIncludeFileSet) {
+                    if (!unusedIncludeASRepository.isUsedInclude(codeFile.getId(), headFileUnusedIncludeFile.getId())) {
+                        codeFileUnusedIncludeFileSet.add(headFileUnusedIncludeFile);
+                    }
+                }
+                //更新最终结果，若没有该源文件则创建实例
+                for (UnusedInclude codeUnusedInclude : codeFileUnusedIncludeSet) {
+                    if (codeFile.getId().equals(codeUnusedInclude.getCoreFile().getId())) {
+                        codeFileUnusedIncludeFileSet.addAll(codeUnusedInclude.getUnusedIncludeFiles());
+                        codeFileUnusedIncludeSet.remove(codeUnusedInclude);
+                        break;
+                    }
+                }
+                codeFileUnusedIncludeSet.add(new UnusedInclude(codeFile, codeFileUnusedIncludeFileSet));
+            }
+        }
+        for (UnusedInclude codeUnusedInclude : codeFileUnusedIncludeSet) {
+            Project project = containRelationService.findFileBelongToProject(codeUnusedInclude.getCoreFile());
             if (project != null) {
                 List<UnusedInclude> unusedIncludeList = result.getOrDefault(project.getId(), new ArrayList<>());
-                unusedIncludeList.add(new UnusedInclude(file, unusedIncludeASRepository.findUnusedIncludeByFileId(file.getId())));
+                unusedIncludeList.add(codeUnusedInclude);
                 result.put(project.getId(), unusedIncludeList);
             }
         }
