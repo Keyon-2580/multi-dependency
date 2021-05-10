1242449bce2c8b08d0bd413a77a0b2e5132f9eee
Kingsley <2901704946@qq.com>
2021-05-10 14:58:15 +0800

    修改Unused Include检测策略，只检测直接未使用的引用

--- src/main/java/cn/edu/fudan/se/multidependency/service/query/smell/impl/UnusedIncludeDetectorImpl.java
+++ src/main/java/cn/edu/fudan/se/multidependency/service/query/smell/impl/UnusedIncludeDetectorImpl.java
@@ -69,51 +69,51 @@ public class UnusedIncludeDetectorImpl implements UnusedIncludeDetector {
     @Override
     public Map<Long, List<UnusedInclude>> detectFileUnusedInclude() {
         Map<Long, List<UnusedInclude>> result = new HashMap<>();
-        Set<UnusedInclude> headFileUnusedIncludeSet = new HashSet<>();
+//        Set<UnusedInclude> headFileUnusedIncludeSet = new HashSet<>();
         Set<UnusedInclude> codeFileUnusedIncludeSet = new HashSet<>();
-        headFileUnusedIncludeSet.addAll(unusedIncludeASRepository.findUnusedIncludeWithSuffix(".h"));
-        headFileUnusedIncludeSet.addAll(unusedIncludeASRepository.findUnusedIncludeWithSuffix(".hpp"));
+//        headFileUnusedIncludeSet.addAll(unusedIncludeASRepository.findUnusedIncludeWithSuffix(".h"));
+//        headFileUnusedIncludeSet.addAll(unusedIncludeASRepository.findUnusedIncludeWithSuffix(".hpp"));
         codeFileUnusedIncludeSet.addAll(unusedIncludeASRepository.findUnusedIncludeWithSuffix(".c"));
         codeFileUnusedIncludeSet.addAll(unusedIncludeASRepository.findUnusedIncludeWithSuffix(".cc"));
         codeFileUnusedIncludeSet.addAll(unusedIncludeASRepository.findUnusedIncludeWithSuffix(".cpp"));
-        for (UnusedInclude headFileUnusedInclude : headFileUnusedIncludeSet) {
-            ProjectFile headFile = headFileUnusedInclude.getCoreFile();
-            //无后缀名的头文件名
-            String headFileNameWithoutSuffix = headFile.getName().substring(0, headFile.getName().length() - headFile.getSuffix().length());
-            Set<ProjectFile> allFiles = new HashSet<>(unusedIncludeASRepository.findFileByHeadFileId(headFile.getId()));
-            Set<ProjectFile> codeFiles = new HashSet<>();
-            //获取当前头文件对应的源文件，条件：名字相同、引用该同文件、后缀为.c/.cc/.cpp
-            for (ProjectFile file : allFiles) {
-                String codeFileNameWithoutSuffix = file.getName().substring(0, file.getName().length() - file.getSuffix().length());
-                if (headFileNameWithoutSuffix.equals(codeFileNameWithoutSuffix) && (file.getSuffix().equals(".c") || file.getSuffix().equals(".cc") || file.getSuffix().equals(".cpp"))) {
-                    codeFiles.add(file);
-                }
-            }
-            if (!codeFiles.isEmpty()) {
-                //任取一个元素做为该头文件的对应源文件，【待改】
-                ProjectFile codeFile = codeFiles.iterator().next();
-                Set<ProjectFile> headFileUnusedIncludeFileSet = new HashSet<>(headFileUnusedInclude.getUnusedIncludeFiles());
-                Set<ProjectFile> codeFileUnusedIncludeFileSet = new HashSet<>();
-                //获取该头文件无效引用中在源文件依旧无效的引用
-                for (ProjectFile headFileUnusedIncludeFile : headFileUnusedIncludeFileSet) {
-                    Boolean isUsedFile = unusedIncludeASRepository.isUsedFile(codeFile.getId(), headFileUnusedIncludeFile.getId());
-                    if (isUsedFile == null || !isUsedFile) {
-                        codeFileUnusedIncludeFileSet.add(headFileUnusedIncludeFile);
-                    }
-                }
-                //更新最终结果，若没有该源文件则创建实例
-                for (UnusedInclude codeUnusedInclude : codeFileUnusedIncludeSet) {
-                    if (codeFile.getId().equals(codeUnusedInclude.getCoreFile().getId())) {
-                        codeFileUnusedIncludeFileSet.addAll(codeUnusedInclude.getUnusedIncludeFiles());
-                        codeFileUnusedIncludeSet.remove(codeUnusedInclude);
-                        break;
-                    }
-                }
-                if (codeFileUnusedIncludeFileSet.size() > 0) {
-                    codeFileUnusedIncludeSet.add(new UnusedInclude(codeFile, codeFileUnusedIncludeFileSet));
-                }
-            }
-        }
+//        for (UnusedInclude headFileUnusedInclude : headFileUnusedIncludeSet) {
+//            ProjectFile headFile = headFileUnusedInclude.getCoreFile();
+//            //无后缀名的头文件名
+//            String headFileNameWithoutSuffix = headFile.getName().substring(0, headFile.getName().length() - headFile.getSuffix().length());
+//            Set<ProjectFile> allFiles = new HashSet<>(unusedIncludeASRepository.findFileByHeadFileId(headFile.getId()));
+//            Set<ProjectFile> codeFiles = new HashSet<>();
+//            //获取当前头文件对应的源文件，条件：名字相同、引用该同文件、后缀为.c/.cc/.cpp
+//            for (ProjectFile file : allFiles) {
+//                String codeFileNameWithoutSuffix = file.getName().substring(0, file.getName().length() - file.getSuffix().length());
+//                if (headFileNameWithoutSuffix.equals(codeFileNameWithoutSuffix) && (file.getSuffix().equals(".c") || file.getSuffix().equals(".cc") || file.getSuffix().equals(".cpp"))) {
+//                    codeFiles.add(file);
+//                }
+//            }
+//            if (!codeFiles.isEmpty()) {
+//                //任取一个元素做为该头文件的对应源文件，【待改】
+//                ProjectFile codeFile = codeFiles.iterator().next();
+//                Set<ProjectFile> headFileUnusedIncludeFileSet = new HashSet<>(headFileUnusedInclude.getUnusedIncludeFiles());
+//                Set<ProjectFile> codeFileUnusedIncludeFileSet = new HashSet<>();
+//                //获取该头文件无效引用中在源文件依旧无效的引用
+//                for (ProjectFile headFileUnusedIncludeFile : headFileUnusedIncludeFileSet) {
+//                    Boolean isUsedFile = unusedIncludeASRepository.isUsedFile(codeFile.getId(), headFileUnusedIncludeFile.getId());
+//                    if (isUsedFile == null || !isUsedFile) {
+//                        codeFileUnusedIncludeFileSet.add(headFileUnusedIncludeFile);
+//                    }
+//                }
+//                //更新最终结果，若没有该源文件则创建实例
+//                for (UnusedInclude codeUnusedInclude : codeFileUnusedIncludeSet) {
+//                    if (codeFile.getId().equals(codeUnusedInclude.getCoreFile().getId())) {
+//                        codeFileUnusedIncludeFileSet.addAll(codeUnusedInclude.getUnusedIncludeFiles());
+//                        codeFileUnusedIncludeSet.remove(codeUnusedInclude);
+//                        break;
+//                    }
+//                }
+//                if (codeFileUnusedIncludeFileSet.size() > 0) {
+//                    codeFileUnusedIncludeSet.add(new UnusedInclude(codeFile, codeFileUnusedIncludeFileSet));
+//                }
+//            }
+//        }
         List<UnusedInclude> codeFileUnusedIncludeList = new ArrayList<>(codeFileUnusedIncludeSet);
         codeFileUnusedIncludeList.sort((u1, u2) -> Integer.compare(u2.getUnusedIncludeFiles().size(), u1.getUnusedIncludeFiles().size()));
         for (UnusedInclude codeUnusedInclude : codeFileUnusedIncludeList) {
