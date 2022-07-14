7125f9cb6e087f1d28ad705631bea07dccd25e68
Fulco <7754574+fulco813@user.noreply.gitee.com>
2022-07-14 20:45:50 +0800

    cal coupling Value test

--- src/test/java/cn/edu/fudan/se/multidependency/service/coupling/CalGroupCouplingValueTest.java
+++ src/test/java/cn/edu/fudan/se/multidependency/service/coupling/CalGroupCouplingValueTest.java
@@ -13,18 +13,20 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Slf4j
 @Transactional
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = MultipleDependencyApp.class)
-class CalCouplingValueTest {
+class CalGroupCouplingValueTest {
     @Autowired
     private final CouplingService couplingService;
 
     @Autowired
-    public CalCouplingValueTest(CouplingService couplingService) {
+    public CalGroupCouplingValueTest(CouplingService couplingService) {
         this.couplingService = couplingService;
     }
 
@@ -37,7 +39,16 @@ class CalCouplingValueTest {
     }
 
     @Test
-    public void calCouplingValue() throws IOException {
-//        couplingService.calCouplingValue("/Users/fulco/Desktop/Data/FDUPro/coupling/coupling_value_export.csv");
+    public void calGroupCouplingValue() throws IOException {
+//        long[] fileIdList = {337,336,335,334,333,330,329,328,327,326,325};
+        long[] fileIdList = {19001,19002,19003,19004,19005,19006,19007,19008,19009,19010,19011};
+//        long[] fileIdList = {5988,5985,5986,5989,5977,5978,5979,5980,5981,5987,5982};
+        List<Long> idList = new ArrayList<>();
+
+        for(long fileId : fileIdList){
+            idList.add(fileId);
+        }
+//        couplingService.calGroupCouplingValue(idList, "/Users/fulco/Desktop/Data/FDUPro/coupling/before.csv");
+        couplingService.calGroupInstablity(idList);
     }
 }
