4bc43206ecb555f494b71c5f220c9487f193cf2f
Keyon-2580 <xiepeich123@163.com>
2022-10-21 15:41:50 +0800

    feat : WMC AMC

--- src/main/java/cn/edu/fudan/se/multidependency/JavaNcssAnalyse.java
+++ src/main/java/cn/edu/fudan/se/multidependency/JavaNcssAnalyse.java
@@ -1,6 +1,5 @@
 package cn.edu.fudan.se.multidependency;
 import cn.edu.fudan.se.multidependency.model.MethodMetric;
-import cn.edu.fudan.se.multidependency.service.insert.ThreadService;
 import cn.edu.fudan.se.multidependency.utils.CsvExportUtil;
 import cn.edu.fudan.se.multidependency.utils.DirExplorer;
 import cn.edu.fudan.se.multidependency.utils.JSONUtil;
@@ -9,12 +8,12 @@ import cn.edu.fudan.se.multidependency.utils.config.JSONConfigFile;
 import cn.edu.fudan.se.multidependency.utils.config.ProjectConfig;
 import cn.edu.fudan.se.multidependency.utils.config.ProjectConfigUtil;
 import javancss.Javancss;
+import org.springframework.beans.factory.annotation.Value;
 
 
 import java.io.File;
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
+import java.text.DecimalFormat;
+import java.util.*;
 import java.util.stream.Collectors;
 
 
@@ -26,6 +25,10 @@ import java.util.stream.Collectors;
 
 public class JavaNcssAnalyse {
 
+  //  @Value("${packages}")
+    private static String packages = "/java/org/apache/";
+
+
 
     public static void main(String[] args) throws Exception {
         YamlUtil.YamlObject yaml = YamlUtil.getYaml(args);
@@ -34,13 +37,13 @@ public class JavaNcssAnalyse {
             String repoPath = projectConfig.getPath();
             System.out.println(repoPath);
             if (repoPath != null && !"".equals(repoPath)) {
-                exportCSV(repoPath + "metric.csv", getAllMethodMetric(repoPath));
+                List<MethodMetric> methodMetricList = getAllMethodMetric(repoPath);
+                exportCSV(repoPath + "metric.csv", methodMetricList);
+                getPackagesCcn(methodMetricList);
             } else {
                 System.out.println("repo path is null");
             }
         }
-
-
     }
 
 
@@ -55,5 +58,47 @@ public class JavaNcssAnalyse {
     public static void exportCSV(String tableName, List<MethodMetric> methodMetrics) throws Exception {
         List<String[]> contents = methodMetrics.stream().map((m) -> new String[]{m.getRelativeFilePath(), m.getMethodFullName(), String.valueOf(m.getMethodCcn()), String.valueOf(m.getNcss())}).collect(Collectors.toList());
         CsvExportUtil.doExport(tableName, contents, "relativeFilePath", "methodFullName", "methodCcn", "ncss");
+    }
+
+    //文件 包聚合
+    public static Map<String, Map<String, Double>> getPackagesCcn(List<MethodMetric> allMethodMetric){
+        allMethodMetric = allMethodMetric.stream().filter(methodMetric -> methodMetric.getNcss() > 5).collect(Collectors.toList());
+        DecimalFormat df = new DecimalFormat("#.00");
+        Map<String, List<MethodMetric>> groupByFiles;
+        groupByFiles = allMethodMetric.stream().collect(Collectors.groupingBy(MethodMetric::getRelativeFilePath));
+        Map<String, List<Double>> fileToCcn = new HashMap<>();
+        //文件聚合
+        for(Map.Entry<String, List<MethodMetric>> entry : groupByFiles.entrySet()){
+            List<Double> list = new ArrayList<>();
+            int totalCcn = entry.getValue().stream().mapToInt(MethodMetric::getMethodCcn).sum();
+            int count = entry.getValue().size();
+            list.add((double) totalCcn);
+            list.add( Double.valueOf(df.format((double) totalCcn / count)));
+            fileToCcn.put(entry.getKey(), list);
+        }
+        Map<String, Map<String, Double>> packageToCcn = new HashMap<>();
+        List<String> packageList = Arrays.stream(packages.split(",")).collect(Collectors.toList());
+        //包聚合
+        for(String onePackage : packageList){
+            Map<String, Double> dataMap = new HashMap<>();
+            Double WMC = 0.0;
+            Double AMC = 0.0;
+            int filesNum = 0;
+            for(Map.Entry<String, List<Double>> entry : fileToCcn.entrySet()){
+                if(entry.getKey().startsWith(packages)){
+                    filesNum++;
+                    WMC += entry.getValue().get(0);
+                    AMC += entry.getValue().get(1);
+                }
+            }
+            dataMap.put("WMC", WMC);
+            dataMap.put("AMC", Double.valueOf(df.format(AMC)));
+            dataMap.put("AWMC", Double.valueOf(df.format(WMC / filesNum)));
+            dataMap.put("AAMC", Double.valueOf(df.format(AMC /filesNum)));
+            packageToCcn.put(onePackage, dataMap);
+        }
+        System.out.println(packageToCcn);
+        return packageToCcn;
+
     }
 }
